db680e814a732369a05a967608e8ed08de8e44f7
Deilons <apolo4748@gmail.com>
2024-08-05 07:14:23 -0500

    Feat: Veterinary CLass propietties and some methods

--- Models/VeterinaryClinic.cs
+++ Models/VeterinaryClinic.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Veterinary_Center.Models
-{
-    public class VeterinaryClinic
+namespace Veterinary_Center.Models;
+
+public class VeterinaryClinic
+{   
+    // properties
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+
+    public List<Dog> Dogs = new List<Dog>();
+
+    public List<Cat> Cats = new List<Cat>();
+
+    // constructor
+    public VeterinaryClinic(string name, string address)
     {
-        
+        Name = name;
+        Address = address;
     }
+
+    // methods
+
+    public void SaveDog(Dog newDog)
+    {
+        Dogs.Add(newDog);
+    }
+
+    public void SaveCat(Cat newCat)
+    {
+        Cats.Add(newCat);
+    }
+    /*
+    public void UpdateDog(Dog dog)
+    {
+        Console.WriteLine("Enter the Id of the animal you want to update");
+        int AnimalId = int.Parse(Console.ReadLine());
+        
+        if (dog.Id == AnimalId)
+        {
+            Console.WriteLine("Enter the new name of the animal");
+            string newName = Console.ReadLine();
+            dog.Name = newName;
+        }
+    }
+    */
+
 }
