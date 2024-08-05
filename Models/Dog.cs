17f618a10432ea5574ed89066fb0b5c2d852004a
Deilons <apolo4748@gmail.com>
2024-08-05 07:59:26 -0500

    Fix: createDog method in Dog class now is complete and works correctly

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -75,34 +75,82 @@ public class Dog : Animal
     }
 
     public static Dog CreateDog()
-    {
-        int id = IdCounter;
+    {   
         IdCounter++;
+        int id = IdCounter;
 
         Console.WriteLine("Enter the name of the animal");
         string name = Console.ReadLine();
         if ( string.IsNullOrWhiteSpace(name))
         {
-            while (!string.IsNullOrWhiteSpace(name))
-            Console.WriteLine("The name cannot be empty, please enter a valid name");
-            name = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(name))
+            {   
+                Console.WriteLine("The name cannot be empty, please enter a valid name");
+                name = Console.ReadLine();
+            }
+            
         }
 
         Console.WriteLine("Enter the birth date of the animal");
         DateOnly birthDate = DateOnly.Parse(Console.ReadLine());
+        if (birthDate > DateOnly.FromDateTime(DateTime.Now))
+        {
+            while (birthDate > DateOnly.FromDateTime(DateTime.Now))
+            {
+                Console.WriteLine("The birth date cannot be in the future, please enter a valid birth date");
+                birthDate = DateOnly.Parse(Console.ReadLine());
+            }
+        }
 
         Console.WriteLine("Enter the breed of the animal");
         string breed = Console.ReadLine();
 
+        if (string.IsNullOrWhiteSpace(breed))
+        {
+            while (string.IsNullOrWhiteSpace(breed))
+            {
+                Console.Clear();
+                Console.WriteLine("The breed cannot be empty, please enter a valid breed");
+                breed = Console.ReadLine();
+            }
+        }
+
         Console.WriteLine("Enter the color of the animal");
         string color = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(color))
+        {
+            while (string.IsNullOrWhiteSpace(color))
+            {
+                Console.Clear();
+                Console.WriteLine("The color cannot be empty, please enter a valid color");
+                color = Console.ReadLine();
+            }
+        }
 
         Console.WriteLine("Enter the weight of the animal");
         double weightInKg = double.Parse(Console.ReadLine());
 
+        if (weightInKg <= 0)
+        {
+            while (weightInKg <= 0)
+            {
+                Console.WriteLine("The weight cannot be less than zero, please enter a valid weight");
+                weightInKg = double.Parse(Console.ReadLine());
+            }
+        }
+
         Console.WriteLine("Is the animal castrated? Y/N");
-        string answer = Console.ReadLine();
+        string answer = Console.ReadLine().ToUpper();
         bool isCastrated;
+        if (string.IsNullOrWhiteSpace(answer))
+        {
+            while (string.IsNullOrWhiteSpace(answer) || answer != "Y" || answer != "N")
+            {
+                Console.WriteLine("The answer cannot be empty, please enter a valid answer");
+                answer = Console.ReadLine();
+            }
+        }
         if (answer == "Y")
         {
             isCastrated = true;
@@ -121,6 +169,15 @@ public class Dog : Animal
                         ");
         int choice1 = int.Parse(Console.ReadLine());
         string temperament = null;
+
+        if (choice1 > 3 || choice1 < 1 || choice1.ToString() == null)
+        {
+            while (choice1 > 3 || choice1 < 1 || choice1.ToString() == null)
+            {
+                Console.WriteLine("The answer cannot be empty, please enter a valid answer");
+                choice1 = int.Parse(Console.ReadLine());
+            }
+        }
         switch (choice1)
         {
             case 1:
@@ -132,13 +189,32 @@ public class Dog : Animal
             case 3:
                 temperament = "Aggressive";
                 break;
+            default:
+                break;
         }
 
         Console.WriteLine("Enter the microchip number of the animal");
         string microchipNumber = Console.ReadLine();
 
+        if (string.IsNullOrWhiteSpace(microchipNumber))
+        {
+            while (string.IsNullOrWhiteSpace(microchipNumber))
+            {
+                Console.WriteLine("The microchip number cannot be empty, please enter a valid microchip number");
+                microchipNumber = Console.ReadLine();
+            }
+        }
+
         Console.WriteLine("Enter the bark volume of the animal");
         string barkVolume = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(barkVolume))
+        {
+            while (string.IsNullOrWhiteSpace(barkVolume))
+            {
+                Console.WriteLine("The bark volume cannot be empty, please enter a valid bark volume");
+                barkVolume = Console.ReadLine();
+            }
+        }
 
         Console.WriteLine("Enter the coat type of the animal");
         Console.WriteLine(@"
@@ -149,6 +225,14 @@ public class Dog : Animal
                         ");
         int choice2 = int.Parse(Console.ReadLine());
         string coatType = null;
+        if (choice2 > 4 || choice2 < 1 || choice1.ToString() == null)
+        {
+            while (choice2 > 4 || choice2 < 1 || choice1.ToString() == null)
+            {
+                Console.WriteLine("The answer cannot be empty, please enter a valid answer");
+                choice2 = int.Parse(Console.ReadLine());
+            }
+        }
         switch (choice2)
         {
             case 1:
